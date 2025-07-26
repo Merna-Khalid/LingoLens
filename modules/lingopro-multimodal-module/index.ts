cfc66af91142986dc07560a19c062564b646b199
Merna <merna.khaled.5049@gmail.com>
2025-07-26 21:39:31 +0300

    Fixed native-module binding

--- modules/lingopro-multimodal-module/index.ts
+++ modules/lingopro-multimodal-module/index.ts
@@ -1,6 +1,11 @@
 // Reexport the native module. On web, it will be resolved to LingoproMultimodalModule.web.ts
 // and on native platforms to LingoproMultimodalModule.ts
-export * from './src/LingoproMultiModalModule';
+//export * from './src/LingoproMultiModalModule';
 // export { default as LingoproMultimodalModuleView } from './src/LingoproMultimodalModuleView';
-export * from './src/LingoproMultiModal.types';
+// export * from './src/LingoproMultiModal.types';
 
+//import { requireNativeModule } from 'expo-modules-core';
+// export default requireNativeModule('LingoproMultimodal');
+import { NativeModulesProxy } from 'expo-modules-core';
+
+export default NativeModulesProxy.LingoproMultimodal;
